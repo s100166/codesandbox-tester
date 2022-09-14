c66f250a280a9d0476b17cb134c17ddda8d34f19
NxiM <s100166@ohmymi.com>
2022-09-14 03:19:12 +0000

    Update 3 files

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+import currencys from "./currencys.json";
+
+app.get("/currencys", (req, res) => {
+    res.send(currencys);
 })
 
 app.listen(process.env.PORT, () => {
-    console.log(`App listening on port ${process.env.PORT}`);
+    console.log(`Tester app listening on port https://1eliee-3000.preview.csb.app/${process.env.PORT}`);
 })
