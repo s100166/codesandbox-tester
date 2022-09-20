e20779eaccbc147c09aa21fc57f9094e87fa2c9b
NxiM <s100166@ohmymi.com>
2022-09-20 09:46:03 +0000

    Update 6 files

--- api/src/routes/currencys.ts
+++ api/src/routes/currencys.ts
@@ -6,10 +6,28 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     res.send('Currencys API');
-})
+});
 
-router.get(PATH.CURRENCYS, (req, res) => {
+router.get(PATH.CURRENCYS.LIST, (req, res) => {
     res.status(200).send(currencys);
-})
+});
+
+router.get(PATH.CURRENCYS.GET, (req, res) => {
+    const data: any[] = [];
+    const { code } = req.query;
+    
+    for (const currency of currencys) {
+        if (currency.code === code) {
+            data.push(currency);
+            break;
+        }
+    }
+    
+    if (data && !data.length) {
+        return res.status(404).json({ status: res.statusCode, message: "Provided code not found" });
+    }
+    
+    return res.status(200).json({ status: res.statusCode, data: Object.assign({}, ...data) });
+});
 
 export default router;
